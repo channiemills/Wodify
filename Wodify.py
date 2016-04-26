b9aa95ed4936a62611f5796656898bb66aa36176
Chantel Miller <cmiller20@gmail.com>
2016-04-25 21:50:23 -0500

    Cleaned up to use check for measure. Removed comments. Created login function.

--- Wodify.py
+++ Wodify.py
@@ -9,21 +9,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import StaleElementReferenceException
 
 
 browser = webdriver.Chrome()
 
-browser.get('https://app.wodify.com/Performance/AthletePerformanceCardEntry.aspx')
 
-username_field = browser.find_element_by_name('wt73$wtMainContent$wtUserNameInput')
-username_field.send_keys(username)
-password_field = browser.find_element_by_name('wt73$wtMainContent$wtPasswordInput')
-password_field.send_keys(password)
-#submit
-browser.find_element_by_id('wt73_wtMainContent_wt59').click()
-
-#time.sleep(2)
+def login():
+    browser.get('https://app.wodify.com/Performance/AthletePerformanceCardEntry.aspx')
+    username_field = browser.find_element_by_name('wt73$wtMainContent$wtUserNameInput')
+    username_field.send_keys(username)
+    password_field = browser.find_element_by_name('wt73$wtMainContent$wtPasswordInput')
+    password_field.send_keys(password)
+    browser.find_element_by_id('wt73_wtMainContent_wt59').click()
 
 
 #writing a function to wait for elements
@@ -41,17 +38,12 @@ def element_wait(tag, element):
     except TimeoutException:
         browser.quit()
 
+login()
 
 #open athlete dropdown
 element_wait(By.ID, "W_Theme_UI_wt19_block_wtMainContent_wtAthleteDropDown_chosen").click()
 
 
-#former open athlete dropdown
-#athlete_dropdown = browser.find_element_by_id('W_Theme_UI_wt19_block_wtMainContent_wtAthleteDropDown_chosen')
-#athlete_dropdown.click()
-
-
-#time.sleep(2)
 #select athlete
 browser.find_elements_by_class_name('active-result')[1].click()
 #print athlete name
@@ -61,38 +53,12 @@ print athlete_name.text
 #list = browser.find_elements_by_class_name('active-result')
 #print list
 
-#time.sleep(2)
+
 #select type
 
 type_dropdown = element_wait(By.ID, 'W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentTypeComboBox')
 Select(type_dropdown).select_by_visible_text('Weightlifting')
 
-# type_dropdown = Select(browser.find_element_by_id('W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentTypeComboBox'))
-# type_dropdown.select_by_visible_text('Weightlifting')
-
-#time.sleep(2)
-
-# the problem is that the element is clickable but there is nothing in it yet so it closes
-
-# def set_component():
-# # need a function to retry until components are fully loaded
-#     for attempt in range(10):
-#         try:
-#             #open component dropdown
-#             element_wait(By.ID, 'W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen').click()
-#             element = element_wait(By.CSS_SELECTOR, '#W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen > a > span')
-#             #element = browser.find_element_by_css_selector('#W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen > a > span')
-#             input = element_wait(By.CSS_SELECTOR, '#W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen > div > div > input[type="text"]')
-#             #input = browser.find_element_by_css_selector('#W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen > div > div > input[type="text"]')
-#             input.send_keys('Back Squat\n')
-#             break
-#         except AttributeError:
-#             pass
-#         # else:
-#         #     break
-#
-#
-# set_component()
 
 #verify measure text boxes are present before setting component
 
@@ -100,38 +66,24 @@ element_wait(By.NAME, 'W_Theme_UI_wt19$block$wtMainContent$wtPCard$wt44')
 
 #open component dropdown
 
-
 element_wait(By.ID, 'W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen').click()
-#browser.find_element_by_id('W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen').click()
-
-#time.sleep(5)
-#print 'Did you wait?'
-
 
 #select movement
 #will need to iterate input over list of expected movements
 
 
 element = element_wait(By.CSS_SELECTOR, '#W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen > a > span')
-# #element = browser.find_element_by_css_selector('#W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen > a > span')
 input = element_wait(By.CSS_SELECTOR, '#W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen > div > div > input[type="text"]')
-# #input = browser.find_element_by_css_selector('#W_Theme_UI_wt19_block_wtMainContent_wtPCard_wtComponentDropDown_chosen > div > div > input[type="text"]')
 input.send_keys('Back Squat\n')
 
 print 'Back Squat'
 
 
-time.sleep(3)
-
-
 #get 1 rep max
-#get_max = browser.find_element_by_css_selector('#W_Theme_UI_wt19_block_wtMainContent_wtPCard_W_Performance_UI_wt18_block_wtPRWrapper > table > tbody > tr:nth-child(2) > td:nth-child(1)')
-#get_max = browser.find_element_by_css_selector('//*[@id="W_Theme_UI_wt19_block_wtMainContent_wtPCard_W_Performance_UI_wt18_block_wtPRWrapper"]/table/tbody/tr[2]/td[1]')
-get_max = browser.find_element_by_xpath('//*[@id="W_Theme_UI_wt19_block_wtMainContent_wtPCard_W_Performance_UI_wt18_block_wtPRWrapper"]/table/tbody/tr[2]/td[1]')
+get_max = element_wait(By.XPATH, '//*[@id="W_Theme_UI_wt19_block_wtMainContent_wtPCard_W_Performance_UI_wt18_block_wtPRWrapper"]/table/tbody/tr[2]/td[1]')
 max_value = get_max.text
 
 print max_value
 
-time.sleep(2)
 
 browser.quit()
